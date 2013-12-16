76f191ef50666d2d60a3fd49ed04eb8a8d114b2c
laht <jon.joakim@gmail.com>
2013-12-16 03:15:49 +0100

    Laboration 2 commit #2

---
+++ labby/labby/sec.php
@@ -0,0 +1,105 @@
+<?php
+require_once("password.php");
+/**
+* Just som simple scripts for session handling
+*/
+function sec_session_start() {
+        $session_name = 'sec_session_id'; // Set a custom session name
+        $secure = false; // Set to true if using https.
+        $httponly = true; // This stops javascript being able to access the session id. 
+        ini_set('session.use_only_cookies', 1); // Forces sessions to only use cookies. 
+        $cookieParams = session_get_cookie_params(); // Gets current cookies params.
+        session_set_cookie_params(3600, $cookieParams["path"], $cookieParams["domain"], $secure, $httponly); 
+        session_name($session_name); // Sets the session name to the one set above.
+        session_start(); // Start the php session
+        session_regenerate_id(); // regenerated the session, delete the old one.  
+}
+
+function validateSession() {
+	if ($_SESSION['userAgent'] != $_SERVER['HTTP_USER_AGENT']) {
+		header('HTTP/1.1 401 Unauthorized'); die();
+	}
+}
+function checkUser() {
+	if(!session_id()) {
+		sec_session_start();
+	}
+	if(!isset($_SESSION["user"])) {header('HTTP/1.1 401 Unauthorized'); die();}
+	$user = getUser($_SESSION["user"]);
+	$un = $user[0]["username"];
+
+	if(isset($_SESSION['login_string'])) {
+		if($_SESSION['login_string'] !== hash('sha512', "Come_On_You_Spurs" + $un) ) {
+			header('HTTP/1.1 401 Unauthorized'); die(); // Yey!
+		}
+	}
+	else {
+		header('HTTP/1.1 401 Unauthorized'); die();
+	}
+}
+
+function isUser($u, $p) {
+	$db = null;
+	try {
+		$db = new PDO("sqlite:db.db");
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	}
+	catch(PDOEception $e) {
+		die("Del -> " .$e->getMessage());
+	}
+	$q = "SELECT username, password FROM users WHERE username = '$u'";
+
+	$result;
+	$stm;	
+	try {
+		$stm = $db->prepare($q);
+		$stm->execute();
+		$result = $stm->fetchAll();
+	}
+	catch(PDOException $e) {
+		echo("Error creating query: " .$e->getMessage());
+		return false;
+	}
+	if(password_verify($p, $result[0]["password"]) && $u == $result[0]["username"])
+		return true;
+	else
+	 	return false;
+	
+}
+
+function getUser($user) {
+	$db = null;
+
+	try {
+		$db = new PDO("sqlite:db.db");
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	}
+	catch(PDOEception $e) {
+		die("Del -> " .$e->getMessage());
+	}
+	$q = "SELECT * FROM users WHERE username = '$user'";
+	
+	$result;
+	$stm;	
+	try {
+		$stm = $db->prepare($q);
+		$stm->execute();
+		$result = $stm->fetchAll();
+	}
+	catch(PDOException $e) {
+		echo("Error creating query: " .$e->getMessage());
+		return false;
+	}
+	
+	return $result;
+}
+
+function logout() {
+	if(!session_id()) {
+		sec_session_start();
+	}
+	$_SESSION = array();
+	session_destroy();
+	echo "index.php";
+}
+
